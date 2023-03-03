97436d0b468aac7aa43d69a2b6bf33827e94edbd
Anton Kovalyov <thebyagent@gmail.com>
2023-03-03 10:23:37 -0800

    Bump version of credential provider downloaded during setup (#55)
    * Bump version of credential provider downloaded during setup.
    
    ---------
    
    Co-authored-by: John Schmeichel <johsch@microsoft.com>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import urllib.request
 
 CREDENTIAL_PROVIDER = (
     "https://github.com/Microsoft/artifacts-credprovider/releases/download/"
-    + "v0.1.22"
+    + "v1.0.1"
     + "/Microsoft.NuGet.CredentialProvider.tar.gz"
 )
 
