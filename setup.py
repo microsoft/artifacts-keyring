bae86ca30b9791cc7cb09aa25fd48d6a75c91081
Emily Bettencourt <embetten@microsoft.com>
2025-02-28 15:28:12 -0800

    init

--- setup.py
+++ setup.py
@@ -13,8 +13,9 @@ import sys
 import tarfile
 import tempfile
 import urllib.request
+import platform
 
-CREDENTIAL_PROVIDER_BASE = "https://github.com/Microsoft/artifacts-credprovider/releases/download/v1.3.0/"
+CREDENTIAL_PROVIDER_BASE = "https://github.com/Microsoft/artifacts-credprovider/releases/download/v1.4.0-alpha/"
 CREDENTIAL_PROVIDER_NETFX = CREDENTIAL_PROVIDER_BASE + "Microsoft.NuGet.CredentialProvider.tar.gz"
 CREDENTIAL_PROVIDER_NET8 = CREDENTIAL_PROVIDER_BASE + "Microsoft.Net8.NuGet.CredentialProvider.tar.gz"
 CREDENTIAL_PROVIDER_NET_VER_VAR_NAME = "ARTIFACTS_KEYRING_USE_NET8"
@@ -41,10 +42,30 @@ def get_version(root):
     m = re.search(r"__version__\s*=\s*['\"](.+?)['\"]", txt)
     return m.group(1) if m else "0.1.0"
 
+def get_rid():
+    os_name = platform.system().lower();
+    architecture = platform.machine().lower()
+
+    if os_name == "windows":
+        return "win-x64"
+    elif os_name == "darwin":
+        return "osx-x64"
+    elif os_name == "linux":
+        return "linux-x64"
+    else:
+        return ""  
+
 def get_download_url():
+    os_name = platform.system().lower();
+    architecture = platform.machine().lower()
+
     use_net_8 = CREDENTIAL_PROVIDER_NET_VER_VAR_NAME in os.environ and \
         os.environ[CREDENTIAL_PROVIDER_NET_VER_VAR_NAME] and \
         str(os.environ[CREDENTIAL_PROVIDER_NET_VER_VAR_NAME]).lower() == "true"
+    
+    rid = get_rid()
+    if rid != "":
+        CREDENTIAL_PROVIDER_NET8 = CREDENTIAL_PROVIDER_BASE + "Microsoft.Net8." + rid + ".NuGet.CredentialProvider.tar.gz"
 
     return CREDENTIAL_PROVIDER_NET8 if use_net_8 else CREDENTIAL_PROVIDER_NETFX
 
